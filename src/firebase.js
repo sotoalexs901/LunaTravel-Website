25367ab92da59cf397e26ceb54214e6401a42728
sotoalexs901 <sotoalexs90@gmail.com>
2025-10-23 21:08:13 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,20 @@
+// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Configuración directa de tu app Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: "AIzaSyBUiJ9u2GyPBovgoaS64r89E1tydCP5Na4",
+  authDomain: "lunatrack-95199.firebaseapp.com",
+  projectId: "lunatrack-95199",
+  storageBucket: "lunatrack-95199.firebasestorage.app",
+  messagingSenderId: "82970120947",
+  appId: "1:82970120947:web:2d7b70d052816802c05d86",
+  measurementId: "G-QCYKV5DXKD"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
 
